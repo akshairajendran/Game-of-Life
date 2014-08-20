2514c0851c7aab23185c309b7c0794b6f3e80005
akshairajendran <akshai.rajendran@gmail.com>
2014-08-20 12:14:29 -0500

    can't make function work, added input to script

--- animate.py
+++ animate.py
@@ -6,21 +6,21 @@ from matplotlib import animation
 import main
 
 #function that takes frame rate n and dimensions r,c
-def animate(n,r,c):
-    #generate figure and initial array
-    fig = plt.figure()
-    Z = main.genarray(r,c)
-    #set image object
-    im = plt.imshow(Z,cmap=plt.get_cmap('jet'))
+#generate figure and initial array
+n = int(raw_input("Framespeed:"))
+r = int(raw_input("Rows:"))
+c = int(raw_input("Columns:"))
+fig = plt.figure()
+Z = main.genarray(r,c)
+#set image object
+im = plt.imshow(Z,cmap=plt.get_cmap('jet'))
 
-    #define function that updates the figure, by evolving Z and setting image array to new Z
-    def updatefig(*args):
-        global Z
-        Z = main.evolve(Z)
-        im.set_array(Z)
-        return im,
-
-    #animate by updating Z at frame rate n
-    ani = animation.FuncAnimation(fig,updatefig,interval=n,blit=True)
-    plt.show()
-    return
+#define function that updates the figure, by evolving Z and setting image array to new Z
+def updatefig(*args):
+    global Z
+    Z = main.evolve(Z)
+    im.set_array(Z)
+    return im,
+#animate by updating Z at frame rate n
+ani = animation.FuncAnimation(fig,updatefig,interval=n,blit=True)
+plt.show()
